bd24377aefb81074657bf90b38c7208295c6e4d0
unknown <roma.komorowska@gmail.com>
2020-09-17 07:08:34 +0200

    update

--- send_form.php
+++ send_form.php
@@ -2,24 +2,24 @@
 header("content-type: application/json; charset=utf-8");
 $name=isset($_POST['name']) ? $_POST['name'] : "";
 $email=isset($_POST['email']) ? $_POST['email'] : "";
-$phone=isset($_POST['phone']) ? $_POST['phone'] : "";
+/*$phone=isset($_POST['phone']) ? $_POST['phone'] : ""; */
 $message=isset($_POST['message']) ? $_POST['message'] : "";
-if($name && $email && $phone && $message){
+if($name && $email && $message){
  $headers = "MIME-Version: 1.0\r\nContent-type: text/plain; charset=utf-8\r\nContent-Transfer-Encoding: 8bit";
- $message_body="Formularz kontaktowy wysłany ze strony www.example.com\n";
+ $message_body="Formularz kontaktowy wysłany ze strony www.polishvibe.pl\n";
  $message_body.="Imię i nazwisko: $name\n";
  $message_body.="Adres email: $email\n";
- $message_body.="Numer telefonu: $phone\n\n";
+ /*$message_body.="Numer telefonu: $phone\n\n"; */
  $message_body.=$message;
- if(mail("roma.komorowska@gmail.com","Formularz kontaktowy",$message_body,$headers)){
- $json=array("status"=>1,"msg"=>"<p class='status_ok'>Twój formularz został pomyślnie wysłany.</p>");
+ if(mail("kontakt@polishvibe.pl","Formularz kontaktowy",$message_body,$headers)){
+ $json=array("status"=>1,"msg"=>"<p class='status_ok'>Thank you! We will contact you soon :)</p>");
  }
  else{
- $json=array("status"=>0,"msg"=>"<p class='status_err'>Wystąpił problem z wysłaniem formularza.</p>"); 
+ $json=array("status"=>0,"msg"=>"<p class='status_err'>An error has occured, please try again later</p>"); 
  }
 }
 else{
- $json=array("status"=>0,"msg"=>"<p class='status_err'>Proszę wypełnić wszystkie pola przed wysłaniem.</p>"); 
+ $json=array("status"=>0,"msg"=>"<p class='status_err'>Please fill all fields before sending</p>"); 
 }
 echo json_encode($json);
 exit;
